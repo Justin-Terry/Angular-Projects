2fdea26a2d3e60eddf7987fc4657b9bee9c59be9
Justin Terry <Justin.Terry@student.csulb.edu>
2019-11-04 14:29:13 -0800

    Finished Part 3

---
+++ Angular8/recipe-app/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+  recipes:Recipe[] = [
+    new Recipe("Test", "This is a test recipe", "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2019/8/6/0/WU2301_Four-Cheese-Pepperoni-Pizzadilla_s4x3.jpg.rend.hgtvcom.826.620.suffix/1565115622965.jpeg")
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
