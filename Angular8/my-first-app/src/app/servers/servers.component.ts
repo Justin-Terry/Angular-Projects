4951071d01c3670d2eb6e3c518412e35937dfc11
Justin Terry <Justin.Terry@student.csulb.edu>
2019-11-04 12:35:05 -0800

    Completed 2.30

--- Angular8/my-first-app/src/app/servers/servers.component.ts
+++ Angular8/my-first-app/src/app/servers/servers.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer:boolean = false;
+  serverCreationStatus:string = "No server was created";
+  serverName:string = 'TEST';
+  serverCreated:boolean = false;
+  servers = ["TestServer", "TestServer 2"];
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+   }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = "Server " + this.serverName + " created";
+    this.servers.push(this.serverName);
+    this.serverCreated = true;
+  }
+
+  onUpdateServerName(event:any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
